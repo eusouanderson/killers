60f8c85cd3d559716014a5f17bab97ffff8790b4
Anderson Rodrigues <eusouanderson@outlook.com>
2025-08-05 11:33:59 -0300

    updated

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,20 @@
-import { createRouter, createWebHistory } from "vue-router";
-import PageHome from "@/components/views/PageHome.vue";
 import PageHistory from "@/components/views/PageHistory.vue";
+import PageHome from "@/components/views/PageHome.vue";
 import PagePlayers from "@/components/views/PagePlayers.vue";
-import PageStats from "@/components/views/PageStats.vue";
 import PageSettings from "@/components/views/PageSettings.vue";
+import PageStats from "@/components/views/PageStats.vue";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes = [
-  { path: '/', name: 'Home', component: PageHome },
-  { path: '/history', name: 'History', component: PageHistory },
-  { path: '/players', name: 'Players', component: PagePlayers },
-  { path: '/stats', name: 'Stats', component: PageStats },
-  { path: '/settings', name: 'Settings', component: PageSettings },
-
+  { path: "/", name: "Home", component: PageHome },
+  { path: "/history", name: "History", component: PageHistory },
+  { path: "/players", name: "Players", component: PagePlayers },
+  { path: "/stats", name: "Stats", component: PageStats },
+  { path: "/settings", name: "Settings", component: PageSettings },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
